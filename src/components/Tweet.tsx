48f9a811ccd9f046c8b0322619e8d87a74c365c7
Yan <yangarridodev@gmail.com>
2025-02-26 21:43:27 -0300

    inicio da criação do corpo da página

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,15 @@
+interface TweetProps{
+    user: string
+    content: string
+    likes?: number
+}
+
+export function Tweet(props: TweetProps) {
+    return(
+        <div className="tweet" style={{borderRadius: 8}}>
+            <strong>{props.user}</strong>
+            <p>{props.content}</p>
+            <button>Like {props.likes ?? 0}</button>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './global.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className='layout'>
+      <aside className='sidebar'>
+        <img src="" alt="" />
+        <nav></nav>
+        <button></button>
+      </aside>
+      <div className="content">
+
+      </div>
+    </div>
   </StrictMode>,
 )
