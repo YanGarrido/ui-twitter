a5f6c4a2d21d766c62fdbe89953dc10c86695b3a
Yan <yangarridodev@gmail.com>
2025-02-28 22:20:05 -0300

    criação de rotas e page Timeline

--- src/main.tsx
+++ src/main.tsx
@@ -2,39 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './global.css'
 
-import { Tweet } from './components/Tweet'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 import { Sidebar } from './components/Sidebar'
-import { Header } from './components/Header'
-import { Separator } from './components/Separator'
 
-const tweets = [
-  'Meu primeiro tweet',
-  'Teste',
-  'Deu certo tweetar!'
-]
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='layout'>
       <Sidebar/>
       <div className="content">
-        <main className='timeline'>
-          <Header title="Home"/>
-
-          <form className='new-tweet-form'>
-            <label htmlFor="tweet">
-              <img src="https://github.com/yangarrido.png" alt="Yan Garrido"/>
-              <textarea name="" id="tweet" placeholder="What's happening"/>
-
-            </label>
-            <button type='submit'>Tweet</button>
-          </form>
-          <Separator/>
-
-         {tweets.map(tweet => {
-          return <Tweet key={tweet} content={tweet}/>
-         })}
-        </main>
+        <RouterProvider router={router} />
+        
 
       </div>
     </div>
